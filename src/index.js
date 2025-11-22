154abfcd31397cc109600462e4fa1576e650de5b
Michael Qualizza <mikequalizza@gmail.com>
2025-11-22 17:16:34 -0600

    Clean up index.js by removing comment
    Removed commented line for clarity.

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-
-// Register your /liquidity route
 app.use('/', liquidityRouter);
 
 app.listen(PORT, () => {
